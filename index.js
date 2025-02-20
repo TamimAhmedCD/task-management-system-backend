3449246564214bbff8bb229c4b062dcc7afbf4fd
Tamim Ahmed <tamim20072@gmail.com>
2025-02-20 17:22:41 +0600

    make api for Create Task. This is api post the new Tasks Data

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 3000
-
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k9pcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -13,7 +12,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -21,7 +20,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const db = client.db('taskly')
+    const tasksCollection = db.collection('tasks')
 
+    // ! CRUD Routes for Tasks
+
+    // Create a Task
+    app.post('/tasks', async (req, res) => {
+      try {
+        const task = req.body;
+        const result = await tasksCollection.insertOne(task)
+        res.status(200).json(result)
+      } catch (err) {
+        res.status(500).json({error: "Failed to create Task"})
+      }
+    })
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
@@ -33,11 +46,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Task Management sever is running')
-})
+app.get("/", (req, res) => {
+  res.send("Task Management sever is running");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on PORT: ${port}`);
-})
+  console.log(`Server running on PORT: ${port}`);
+});
